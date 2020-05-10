04dc1326847cbf34538d3f7134e8dd280124ac5d
Thanh Pham <tpham0123@gmail.com>
2020-05-10 22:10:06 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,5 +9,9 @@ def countdown(number)
 end
 
 def countdown_with_sleep(number)
-
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number = number - 1  
+  end
+  return "HAPPY NEW YEAR!"
 end
